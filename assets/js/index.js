891d0de76ff1213202312df0743fa91b264ae61d
maan12-web <maanrnw@gmail.com>
2025-01-27 15:55:51 +0530

    javascript start

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// first line is always execute first
+console.log("Jay Bharat !!");
+
+
+window.alert("dekh lee pehle");
+
+console.log("before window alert we can show in console log")
+
+document.write("Vande Mataram !");
---
+++ assets/js/variable.js
@@ -0,0 +1,12 @@
+let a = 5; // it is in es6 which is provide high security
+var b = "Aryan"; // var is in es5 its shown in console when write window 
+const pi = 3.14; // also in es6
+
+console.log("10" + 10);
+// plus is used for concatinate and arithmatic
+console.log("10" - 10);
+// minus is used for only arithmatic
+
+console.log(a);
+console.log(b);
+console.log(pi);
