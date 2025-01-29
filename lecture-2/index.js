c3d1592d40aa249fec39ba395d4d623872a8f3af
Om6064 <163402327+Om6064@users.noreply.github.com>
2025-01-29 15:07:52 +0530

    added lecture-2

---
+++ lecture-2/index.js
@@ -0,0 +1,11 @@
+const h1 = document.getElementById("h1");
+const h2 = document.getElementById("h2");
+
+h1.innerHTML = "<marquee>popat</marquee>"; // inner html is used for extract the  html tag 
+
+
+
+h2.innerText = "<marquee>popat</marquee>" // inner text is used for extract the text into html tag 
+
+
+
