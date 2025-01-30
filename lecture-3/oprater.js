b1cbaa76aa9b07714954457680d313123bd5e527
viragbafna <viragbafna111@gmail.com>
2025-01-30 16:43:08 +0530

    added lecture 3

---
+++ lecture-3/oprater.js
@@ -0,0 +1,32 @@
+//arithmatic oprater
+let num1 = prompt("Enter Num1 : ");
+let num2 = prompt("Enter Num2 : ");
+console.log(parseInt(num1) + parseInt(num2));
+console.log(parseInt(num1) - parseInt(num2));
+console.log(parseInt(num1) * parseInt(num2));
+console.log(parseInt(num1) / parseInt(num2));
+console.log(parseInt(num1) % parseInt(num2));
+
+
+//logical oprater
+let a = true,b=false
+console.log(a || b);//true
+console.log(a && b);//false
+
+
+//comparisal oprater
+console.log(num1 < num2)
+console.log(num1 > num2)
+console.log(num1 >= num2)
+console.log(num1 <= num2)
+console.log(num1 != num2)
+console.log(num1 == num2)
+console.log(num1 === num2)
+
+//assigment oprater(==,+=,-=,*=,/=)
+
+
+
+
+
+
---
+++ lecture-3/profile.js
@@ -0,0 +1,8 @@
+let a = prompt("enter your name : ");
+let b = prompt("Enter Course Name : ");
+let c = prompt("Enter Age : ");
+
+
+document.getElementById("name").innerHTML = a
+document.getElementById("course").innerHTML = b
+document.getElementById("age").innerHTML = c
