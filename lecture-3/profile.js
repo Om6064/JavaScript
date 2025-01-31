eca57e1b8fff19e986c19f9598e5763fd7ecb7b1
viragbafna <viragbafna111@gmail.com>
2025-01-31 15:33:17 +0530

    ADDED RNW ID CARD

--- lecture-3/profile.js
+++ lecture-3/profile.js
@@ -1,8 +1,16 @@
-let a = prompt("enter your name : ");
-let b = prompt("Enter Course Name : ");
-let c = prompt("Enter Age : ");
+let a = prompt("enter your grid : ");
+let b = prompt("Enter Starting Date : ");
+let c = prompt("Enter Your Name : ");
+let d = prompt("Enter Your Course : ")
+let f = prompt("Enter Course Duration : ")
+let g = prompt("Enter Mobile No. : ")
+let e = prompt("image URL : ")
 
 
-document.getElementById("name").innerHTML = a
-document.getElementById("course").innerHTML = b
-document.getElementById("age").innerHTML = c
+document.getElementById("name").innerHTML = c
+document.getElementById("course").innerHTML = d
+document.getElementById("date").innerHTML = b
+document.getElementById("grid").innerHTML = a
+document.getElementById("duration").innerHTML = f
+document.getElementById("no").innerHTML = g
+document.getElementById("img").src = e
