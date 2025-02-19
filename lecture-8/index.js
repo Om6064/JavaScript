2470b65f4ce75ecb01fe54d069907f77451f7f60
viragbafna <viragbafna111@gmail.com>
2025-02-19 13:13:30 +0530

    added lecture-8

---
+++ lecture-8/index.js
@@ -0,0 +1,65 @@
+let arr =[10,20,30,40,50]
+// let push = arr.push(70);//push return lenth of the array and and also add some ele which is given in parameter
+// console.log(arr,push);
+
+// let pop = arr.pop()
+// console.log(arr,pop); // pop is given deleted element and also delete last element of the array
+
+
+// now shift and ubshift same as push or pop its return lenth and deleted elemnt 
+
+// let shift = arr.shift() // same as pop at start
+// console.log(arr,shift);
+
+// let unshift = arr.unshift([10,60,"aryan"]) // same as push at start
+// console.log(arr,unshift);
+
+
+//  slice(), splice(), and split().
+// let slice  = arr.slice(2,4) // slice is immuteble its cannot change the array but returnthe you wanted part oof the array
+// console.log(arr,slice);
+
+
+// now splice()
+// let splice = arr.splice(2,2,"aryan",50,36) //splice return deleted values from the array 
+// // its destribution of parameters idx when to you wnt to startv deleting 
+// // then count (how many element you want to delete) by Defult: arr.lenth  And then values to insert
+// console.log(arr,splice);
+
+
+// now split() iit is not accept in array it is in string
+// let str  = "18:26:59";
+// let split = str.split(":") // we can turn string into array by using this
+// console.log(str,split);
+
+//  oposite join() function is Work we can turn array into string 
+// let join= split.join(":")
+// console.log(join);
+
+// sort is use for sort array in asseding order
+// let arr1 = [1,2,3,4,101,102]
+// arr1.sort((a,b) => a-b)
+// console.log(arr1);
+
+
+// sort is use for sort array in desending order
+// let arr1 = [1,2,3,4,101,102]
+// arr1.sort((a,b) => b-a)
+// console.log(arr1);
+
+
+// now we do reverce logic with upper functions
+// let str1 = "CarRac";
+// let rev = str1.toLowerCase().split("").reverse().join("")
+// if (str1.toLowerCase() == rev.toLowerCase()) {
+//     console.log("it is the palindrome !!");
+// }else{
+//     console.log("it is Not !! ");
+    
+// }
+// console.log(rev);
+
+
+
+
+ 
