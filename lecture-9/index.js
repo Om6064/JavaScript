0d68dd0934b7b5611b8a7ef5716acbb18774d66f
viragbafna <viragbafna111@gmail.com>
2025-02-20 18:11:56 +0530

    added pr-5-array

---
+++ lecture-9/index.js
@@ -0,0 +1,25 @@
+// arrays functions
+
+let arr = [11,22,33,55,77]
+
+// let map = arr.map((val,idx,ogarr) => { it is also first class function that return another array if in not value so it return undifened
+
+//         return val
+
+// })
+
+// console.log(map);
+
+// let filter = arr.filter((val,idx,ogarray) => {  its only take true or false and return value of an array  
+//     return val % 2 == 0
+// })
+// console.log(filter);
+
+// foreach method
+
+
+
+
+
+
+
