82c78f410221f70739a0765e988d9ca011af7df3
viragbafna <viragbafna111@gmail.com>
2025-03-03 15:58:02 +0530

    added lecture 17 and some addistion in todo

---
+++ lecture-17/index.js
@@ -0,0 +1,21 @@
+// System Define Execption
+
+// console.log("hello!!!");
+// try {
+//     console.log(a);
+// } catch (error) {
+//     console.error(error.message);
+// }
+// console.log("World !!!");
+
+
+// User Define Execption
+let age = 16;
+
+try {
+    ((age >= 18)) ? console.log("Age is 18 or older") : console.log("Age is less than 18");
+} catch (error) {
+    console.log(error.message);
+}
+
+
