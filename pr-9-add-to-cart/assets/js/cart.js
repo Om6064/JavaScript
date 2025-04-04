cb9e2194f61aacaa9557b37facf0050c17780009
om6064 <aryanpatel.lihodiya@gmail.com>
2025-04-04 15:44:15 +0530

    added whole add to cart but relod functionality is panding

--- pr-9-add-to-cart/assets/js/cart.js
+++ pr-9-add-to-cart/assets/js/cart.js
@@ -1,29 +1,69 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let localArr = JSON.parse(localStorage.getItem("cart")) || [];
-    let checkoutBox = document.getElementById("tablecontent");
 
-   
-   
-    
-    
-    function displayItem() {
-        checkoutBox.innerHTML = "";  
-        
-        if (localArr.length === 0) {
-            checkoutBox.innerHTML = `
-                <img src="./assets/images/empty-cart.png" alt="empty-cart image" width="100%">
-                <p class='text-center'>Your cart is empty</p>`;
-            return;
-        }
 
-        let cartHTML = '';
-        localArr.forEach((item, idx) => {
-            cartHTML += `
+let localArr = JSON.parse(localStorage.getItem("cart")) || [];
+let checkoutBox = document.getElementById("tablecontent");
+let totalAmount = 0;
+let totalCard = document.getElementById("totalCard")
+let hiddenContent = document.getElementById("hiddenContent")
+let counter = document.getElementById("counter")
+counter.innerHTML = localArr.length
+
+
+
+function deleteItem(idx) {
+    localArr.splice(idx, 1)
+    saveCartArray()
+    displayItem()
+    hiddenContent.classList.add("d-none")
+}
+
+function updateQuntity(idx, value) {
+    let newQuntity = localArr[idx].quantity + value;
+
+    if (newQuntity <= 0) {
+        deleteItem(idx)
+    } else {
+        localArr[idx].quantity = newQuntity;
+        saveCartArray()
+        displayItem()
+    }
+}
+function displayItem() {
+    checkoutBox.innerHTML = "";
+    totalAmount = 0;
+
+    if (localArr.length == 0) {
+        checkoutBox.innerHTML = `
+         <div class="d-flex gap-5">
+           <div class="col-8">
+            <img src="https://assets-v2.lottiefiles.com/a/6102a4f8-1176-11ee-bcc5-236dd7d5f88b/aK8IKRE5a3.gif" alt="empty-cart image" width="100%">
+            </div>
+           <div class="col-4">
+
+            <h1 class='text-center fw-bold'>Your cart is empty</h1>
+            </div>
+
+         </div>  
+          <button class="bg-white fw-bold hover-shoping" onclick="window.location = './index.html'">
+            <i class="fa-solid fa-arrow-left hover-left"></i>
+            Continue Shopping
+        </button>     
+        `;
+        hiddenContent.classList.add("d-none")
+        return;
+    }
+   
+
+    let cartHTML = '';
+    localArr.forEach((item, idx) => {
+        let subTotal = item.quantity * item.price;
+        totalAmount += subTotal;
+        cartHTML += `
                 <div class="d-flex flex-wrap justify-content-between pb-4 border-bottom">
-                    <div class="col-12 col-md-6">
+                    <div class="col-12 col-md-6 pt-2">
                         <div class="d-flex">
                             <div class="col-6">
-                                <img src="${item.img}" alt="" class="img-fluid">
+                                <img src="${item.img}" alt="${item.productName}" class="img-h">
                             </div>
                             <div class="col-6 d-flex flex-column justify-content-center">
                                 <p class="m-0 text-body-secondary font-14">${item.discribtion}</p>
@@ -34,41 +74,77 @@ document.addEventListener("DOMContentLoaded", function () {
                     <div class="col-12 col-md-6 d-flex flex-column justify-content-center mt-3 mt-md-0">
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="col-3 mt-2 text-center">
-                                <p class="fw-bold">$${item.price}</p>
+                                <p class="fw-bold">$${subTotal}</p>
                             </div>
                             <div class="col-5">
                                 <div class="d-flex border justify-content-between px-3 py-2 rounded">
-                                    <button class="bg-transparent text-secondary">
+                                    <button class="bg-transparent text-secondary" onclick="updateQuntity(${idx},-1)">
                                         <i class="fa-solid fa-minus"></i>
                                     </button>
                                     <div class="fw-bold">${item.quantity}</div>
-                                    <button class="bg-transparent text-secondary">
+                                    <button class="bg-transparent text-secondary" onclick="updateQuntity(${idx},1)">
                                         <i class="fa-solid fa-plus"></i>
                                     </button>
                                 </div>
                             </div>
                             <div class="col-2 text-center">
-                                <button class="text-secondary bg-transparent" onclick='deleteItem()'>Remove</button>
+                                <button class="text-secondary bg-transparent" onclick="deleteItem(${idx})">Remove</button>
                             </div>
                         </div>
                     </div>
                 </div>`
-        });
 
-      
-        checkoutBox.innerHTML = cartHTML;
-    }
 
-  
-    displayItem();
-    
-    
-    function deleteItem(){
-        console.log("wejteirjgti");
-    }
-   
-    
-    function saveCartArray() {
-        localStorage.setItem("cart", JSON.stringify(localArr));
-    }
-});
+    });
+
+
+    checkoutBox.innerHTML = cartHTML;
+    totalCard.innerHTML = ""
+    totalCard.innerHTML += `
+    <div class="border rounded" id="hiddenContent">
+            <div class="p-40">
+                <div class="d-flex justify-content-between border-bottom mb-4">
+                    <p class="text-secondary fw-bold m-0">Subtotal</p>
+                    <p class="fw-bold m-0">$${totalAmount}</p>
+                </div>
+                <p class="fw-bold mb-2">Shipping</p>
+                <div class="mb-2">
+                    <input type="checkbox" class="assent-color">
+                    <label for="" class="text-secondary font-14">Flat rate</label>
+                </div>
+                <div class="mb-3">
+                    <input type="checkbox" class="assent-color">
+                    <label for="" class="text-secondary font-14">Free shipping</label>
+                </div>
+                <div class="d-flex fw-bold justify-content-between border-bottom mb-4">
+                    <p>Shipping to <span class="text-danger"> NY. </span></p>
+                    <p><i class="fa-solid fa-location-dot text-danger"></i> Change address</p>
+                </div>
+
+                <div class="mt-1">
+                    <div class="d-flex justify-content-between">
+                        <div class="text-body-secondary fw-bold">
+                            Total
+                        </div>
+                        <div class="fw-bold" id="total">
+                            $${totalAmount}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="w-100 text-center my-3  py-3 rounded text-white fw-bold hover-checkout" onclick="window.location = './thankyou.html'">
+            Procced To Check
+        </button>
+        <button class="bg-white fw-bold hover-shoping" onclick="window.location = './index.html'">
+            <i class="fa-solid fa-arrow-left hover-left"></i>
+            Continue Shopping
+        </button>
+`
+}
+
+displayItem();
+
+function saveCartArray() {
+    localStorage.setItem("cart", JSON.stringify(localArr));
+}
--- pr-9-add-to-cart/assets/js/script.js
+++ pr-9-add-to-cart/assets/js/script.js
@@ -3,57 +3,57 @@ let products = [
         id: 1,
         img: "https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img8.webp",
         productName: "Girl's Sport Bra",
-        price: 31.00,
-        discribtion: "Under Ware"
+        price: 19.00,
+        discribtion: "Under Wear"
     },
     {
         id: 2,
         img: "https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img6.webp",
         productName: "Champion Bra",
-        price: 31.00,
-        discribtion: "Under Ware"
+        price: 25.00,
+        discribtion: "Under Wear"
     },
     {
         id: 3,
         img: "https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img11_1.webp",
         productName: "Maidenform Bra",
-        price: 31.00,
-        discribtion: "Under Ware"
+        price: 50.00,
+        discribtion: "Under Wear"
     },
     {
         id: 4,
-        img: "	https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img7.webp",
+        img: "https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img7.webp",
         productName: "Hanes Women's Bra",
-        price: 31.00,
-        discribtion: "Under Ware"
+        price: 75.00,
+        discribtion: "Under Wear"
     },
     {
         id: 5,
-        img: "	https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img8_2.jpg",
+        img: "https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img8_2.jpg",
         productName: "Girl's Sport Bra",
         price: 31.00,
-        discribtion: "Under Ware"
+        discribtion: "Under Wear"
     },
     {
         id: 6,
-        img: "	https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img12.webp",
+        img: "https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img12.webp",
         productName: "Maidenform Bra",
-        price: 31.00,
-        discribtion: "Under Ware"
+        price: 100.00,
+        discribtion: "Under Wear"
     },
     {
         id: 7,
         img: "https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/woman-shirt-338x450.png",
         productName: "Maidenform Bra",
-        price: 31.00,
-        discribtion: "Under Ware"
+        price: 20.00,
+        discribtion: "Under Wear"
     },
     {
         id: 8,
-        img: "	https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img13-1.webp",
+        img: "https://weiboo.pixcelsthemes.com/weiboo/assets/images/hand-picked/slider-img13-1.webp",
         productName: "Hanes Women's Pant",
-        price: 31.00,
-        discribtion: "Under Ware"
+        price: 35.00,
+        discribtion: "Under Wear"
     },
 
 ]
@@ -65,20 +65,31 @@ counter.innerHTML = cartArr.length
 
 function addToCart(productId) {
 
-    let product = cartArr.find((obj) => obj.id === productId);
+    let product = products.find((obj) => obj.id === productId);
 
-    if (product) {
-        product.quantity++;  
+    let productIdx = cartArr.findIndex((obj) => obj.id === productId);
+
+    if (productIdx != -1) {
+        // product.quantity++;
+        cartArr[productIdx] = product;
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "It Is Alrady at cart",
+            footer: ''
+          });
     } else {
-        
-        let productToAdd = products.find((obj) => obj.id === productId);
-        
-        if (productToAdd) {
-            productToAdd.quantity = 1;
-            cartArr.push(productToAdd);
-        }
+        product.quantity = 1;
+        cartArr.push(product);
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Item Added In Cart",
+            showConfirmButton: false,
+            timer: 1500
+          });
     }
-
+    
     localStorage.setItem("cart", JSON.stringify(cartArr));
     counter.innerHTML = cartArr.length;
 }
@@ -102,7 +113,7 @@ products.forEach((product, idx) => {
                                     </div>
                                     <p class="text-body-secondary mb-2">${product.productName}</p>
                                     <div class=" justify-content-between d-flex align-items-center justify-content-center">
-                                        <p class="fw-bold mb-0 ms-0 me-0">$31.00</p>
+                                        <p class="fw-bold mb-0 ms-0 me-0">$${product.price}</p>
                                         <button class="bg-color-icon" onclick = "addToCart(${product.id})">
                                             <i class="fa-solid fa-cart-shopping"></i>
                                         </button>
